dfc60887bb4fa028f976f157c9db6c29d17b1b01
Perm <permsangin@gmail.com>
2025-02-01 16:35:38 +0700

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off ::Change project_gambling.cpp and project_gambling.exe to your own file name!!!!
 g++ project_gambling.cpp -o project_gambling.exe -lsfml-graphics -lsfml-window -lsfml-system
 if %errorlevel% neq 0 (
     echo Compilation failed.
